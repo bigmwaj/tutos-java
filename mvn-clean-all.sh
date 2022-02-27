827df50a1821f32d0faf9a7d398496d75e87c78e
Alain Joel MOUAFO <alainmouafo@gmail.com>
2022-02-27 14:01:13 -0500

    Refactoring

--- mvn-clean-all.sh
+++ mvn-clean-all.sh
@@ -1,7 +1,1 @@
-#!/usr/bin/env bash
-
-#
-# executes mvn clean for all sub directories containing a pom.xml
-#
-
-find -maxdepth 1 -mindepth 1 -type d -exec mvn -f '{}/pom.xml' clean \;
+find . -name "pom.xml" -exec echo "mvn clean -f '{}'" \;
--- tuto-spring-cloud-config-server/src/main/java/com/bigmwaj/tuto/TutoProps.java
+++
@@ -1,5 +0,0 @@
-package com.bigmwaj.tuto;
-
-public class TutoProps {
-
-}
